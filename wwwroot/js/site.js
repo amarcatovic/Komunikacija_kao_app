c470f7331508bf2259bd4208dabc50e50d00dadc
Vedad Karalic <vedad744@gmail.com>
2022-11-12 18:12:49 +0100

    Fix wrong github merge

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,14 +10,57 @@ const configuration = {
 };
 const peerConn = new RTCPeerConnection(configuration);
 
-navigator.mediaDevices.getUserMedia({
+
+connection.start().then(function () {
+    connection.on('created', function (roomId) {
+        console.log('Soba napravljena', roomId);
+        roomNameTxt.disabled = true;
+        createRoomBtn.disabled = true;
+        hasRoomJoined = true;
+        connectionStatusMessage.innerText = 'Ušliste u sobu ' + roomId + '. Čekamo ostle...';
+        myRoomId = roomId;
+        isInitiator = true;
+    });
+
+    connection.on('joined', function (roomId) {
+        console.log('ID se pridružio sobi: ', roomId);
+        myRoomId = roomId;
+        isInitiator = false;
+    });
+
+    connection.on('error', function (message) {
+        alert(message);
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+$(createRoomBtn).click(function () {
+    var name = roomNameTxt.value;
+    connection.invoke("CreateRoom", name).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
+
+$('#roomTable tbody').on('click', 'button', function () {
+    if (hasRoomJoined) {
+        alert('Već ste se pridružili ovoj sobi.');
+    } else {
+        var data = $(roomTable).DataTable().row($(this).parents('tr')).data();
+        connection.invoke("Join", data.RoomId).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+});
+
+/*navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true
     })
     .then(gotStream)
     .catch(function (e) {
         alert('Greška prilikom učitavanja: ' + e.name);
-    }); // vidi radi li tebi
+    });*/ // vidi radi li tebi, RADI OK
 
 // TODO Tim: istražiti free WebRTC servere i konfigurisati po tome
 
